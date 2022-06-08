47f3b8af80f772003f1e86962749df0d5df99a4e
Hasimbola <Hasimbola@PHIDIA.local>
2022-06-08 12:49:49 +0300

    Ajout de nouveux requetes

--- lib/bebemayotte/souscat_requette.ex
+++ lib/bebemayotte/souscat_requette.ex
@@ -2,6 +2,7 @@ defmodule Bebemayotte.SouscatRequette do
   import Ecto.Query, warn: false
   alias Bebemayotte.Repo
   alias Bebemayotte.Souscategorie
+  alias Bebemayotte.CatRequette
 
   # GET SOUSCATEGORIE
   def get_all_souscategorie() do
@@ -28,5 +29,14 @@ defmodule Bebemayotte.SouscatRequette do
             select: sc.nom_souscat
         Repo.one(query)
     end
+  end
+
+  def get_count_scat_by_id_cat(nom_cat) do
+    quer = CatRequette.get_id_cat_by_nom_cat(nom_cat)
+    query =
+      from sc in Souscategorie,
+      where: sc.id_cat == ^quer,
+      select: count(sc.id_souscat)
+    Repo.one(query)
   end
 end
